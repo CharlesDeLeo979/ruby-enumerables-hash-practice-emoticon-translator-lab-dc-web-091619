3e7469aeba6f4e2e5806aa4dc69324d6d3b6e549
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-10 18:30:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 def load_library(path)
   emoticons = YAML.load_file(path)
-  emoticon_hash = hash.new 
+  emoticon_hash = Hash.new 
   
   emoticon_hash["get_emoticon"] = Hash.new
   emoticon_hash["get_meaning"] = Hash.new
