285f64c05694d3eb23a8bdb463fdaef42cfdb2fe
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-10 19:22:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
   result = emoticon_hash["get_emoticon"][emoticon]
   if result == nil 
-    reslut = 
+    reslut = "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
