92b3a50864a6c0fe657681d4d61f958c14ed6ff5
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-10 19:21:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][]
+  result = emoticon_hash["get_emoticon"][emoticon]
 end
 
 def get_english_meaning
